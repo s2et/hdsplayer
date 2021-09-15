b97bd2c3c525e0cc91e1186ce89c5960d08fdffc
s2et <80050008+s2et@users.noreply.github.com>
2021-09-15 18:42:16 +0530

    Update remotly.py

--- remotly.py
+++ remotly.py
@@ -10,13 +10,17 @@ def browse_button(text):
     n=io.StringIO()
     print(cmd)
     ft.storlines("STOR fn.txt",cmd)
+    ft.retrlines("RETR ack.txt",r.write)
+    if len(r.getvalue())==0:
+        Label(root, text = "Not Running",font=('Verdana', 15)).grid(row=3,column=2)
+        ft.storlines("STOR ack.txt",io.BytesIO(b'NotRunning'))
     while 1:
         ft.retrlines("RETR ack.txt",n.write)
         t=n.getvalue()
         if len(t)!= 0:
             if len(t)>10:
                 open("statlog.txt","w").write(t)
-            Label(root, text = "Done",font=('Verdana', 15)).pack(side=TOP,pady=10)
+            Label(root, text = "Done",font=('Verdana', 15)).grid(row=4,column=2)
             sp=io.BytesIO(b'')
             ft.storlines("STOR fn.txt",sp)
             ft.storlines("STOR ack.txt",sp)
@@ -24,10 +28,10 @@ def browse_button(text):
         print("Not")
         
 btn = Button(root,text="Update", command=lambda:browse_button(b"Update"))
-btn.pack(side='left')
-btn = Button(root,text="Test", command=lambda:browse_button(b"Play"))
-btn.pack(side='left')
+btn.grid(row=1,column=3)
+btn = Button(root,text="Test", command=lambda:browse_button(b"Test"))
+btn.grid(row=1,column=1)
 btn = Button(root,text="Stop", command=lambda:browse_button(b"Stop"))
-btn.pack(side='left')
+btn.grid(row=1,column=4)
 btn = Button(root,text="Stat", command=lambda:browse_button(b"Stat"))
-btn.pack(side='left')
+btn.grid(row=1,column=5)
