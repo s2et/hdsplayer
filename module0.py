49de8050b43587bd98e3c2745b93f5a43fcad40a
s2et <80050008+s2et@users.noreply.github.com>
2021-09-27 23:28:49 +0800

    Update module0.py

--- module0.py
+++ module0.py
@@ -1,4 +1,6 @@
-import os,subprocess,time,io,sys,signal
+import subprocess, os, signal, time
+subprocess.Popen("python3 /home/firefly/Documents/altareto/module0ext.py", shell = True)
+time.sleep(2)
 subprocess.Popen("python3 /home/firefly/Documents/altareto/module1.py", shell = True)
 time.sleep(5)
 subprocess.Popen("python3 /home/firefly/Documents/altareto/module2.py", shell = True)
