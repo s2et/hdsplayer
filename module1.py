dd78aeb403f2a7079cd4a06272560ced8af3d2f0
s2et <80050008+s2et@users.noreply.github.com>
2021-10-13 20:08:07 +0530

    Update module1.py

--- module1.py
+++ module1.py
@@ -14,17 +14,24 @@ li=open("/home/firefly/Documents/altareto/tmp.txt","w")
 li.write(str(pid))
 li.close()
 print("tt",tt,len(tt))
-def ply(tt):
-  for data in tt:
+for data in tt:
       duration = subprocess.check_output(['ffprobe', '-i', data, '-show_entries', 'format=duration', '-v', 'quiet', '-of', 'csv=%s' % ("p=0")])
-      media=vlc.Media(data)
+      doc.append(duration)
+print("doc",doc)
+
+def ply(tt):
+  i=0
+  while i<len(tt):
+      media=vlc.Media(tt[i[)
       playa.set_media(media)
       playa.play()
-      if duration==b'N/A\n':
+      if doc[i]==b'N/A\n':
          time.sleep(20)
       else:
-         dur=float(duration.decode().split('\n')[0])
+         dur=float(doc[i].decode().split('\n')[0])
          time.sleep(dur)
+     i+=1
+                           
 def stp():
       playa.stop()
 while 1:
